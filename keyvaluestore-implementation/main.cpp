cd816498bf42bf1c13b293b82d815fd2b1d553ff
benmoon0122 <benjaminjmoon1@gmail.com>
2024-12-19 09:10:50 -0500

    Initial Commit

---
+++ keyvaluestore-implementation/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  keyvaluestore-implementation
+//
+//  Created by Benjamin Moon on 12/19/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
